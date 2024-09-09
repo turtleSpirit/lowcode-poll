ca4ad7c8604c84dab25ce9fd63159cdf3cc8787f
朱春来 <zhuchunlai@cdyoue.com>
2024-09-09 22:49:52 +0800

    feat: 使用useState hook做count加1

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const addCount = () => {
+    setCount(count + 1);
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <button onClick={addCount}>点击+1</button>
+        <p>{count}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
