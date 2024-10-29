12ca2d259e646a41489cd288ac9014696b4a47f9
zhuchunlai <myemail_zhu@163.com>
2024-10-29 18:28:16 +0800

    feat: 路由增加manage、question

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -6,10 +6,13 @@ import Home from '../pages/Home';
 import Login from '../pages/Login';
 import NotFound from '../pages/NotFound';
 import Register from '../pages/Register';
-// import QuestionLayout from '../layouts/QuestionLayout';
-// import ManageLayout from '../layouts/ManageLayout';
-
-// import QuestionList from '../pages/question/List';
+import ManageLayout from '../layouts/ManageLayout';
+import List from '../pages/manage/List';
+import Star from '../pages/manage/Star';
+import Trash from '../pages/manage/Trash';
+import QuestionLayout from '../layouts/QuestionLayout';
+import Edit from '../pages/question/Edit';
+import Stat from '../pages/question/Stat';
 // import QuestionDetail from '../pages/question/Detail';
 // import ManageQuestion from '../pages/manage/Question';
 
@@ -23,18 +26,72 @@ const routes = [
         element: <Home />,
       },
       {
-        path: '/login',
+        path: 'login',
         element: <Login />,
       },
       {
-        path: '/register',
+        path: 'register',
         element: <Register />,
+      },
+      {
+        path: 'manage',
+        element: <ManageLayout />,
+        children: [
+          {
+            path: 'list',
+            element: <List />,
+          },
+          {
+            path: 'star',
+            element: <Star />,
+          },
+          {
+            path: 'trash',
+            element: <Trash />,
+          },
+          // {
+          //   path: 'question',
+          //   element: <QuestionLayout />,
+          //   children: [
+          //     {
+          //       path: 'list',
+          //       element: <QuestionList />,
+          //     },
+          //     {
+          //       path: ':id',
+          //       element: <QuestionDetail />,
+          //     },
+          //     {
+          //       path:'manage',
+          //       element: <ManageQuestion />,
+          //     },
+          //   ],
+          // },
+        ],
       },
       {
         path: '*',
         element: <NotFound />,
       },
     ],
+  },
+  {
+    path: '/question',
+    element: <QuestionLayout />,
+    children: [
+      {
+        path: 'edit/:id',
+        element: <Edit />,
+      },
+      {
+        path: 'stat/:id',
+        element: <Stat />,
+      },
+    ],
+  },
+  {
+    path: '*',
+    element: <NotFound />,
   },
 ];
 const router = createBrowserRouter(routes);
