cff8f170819064c6d81b60982fba1f4806bb8a51
zhuchunlai <myemail_zhu@163.com>
2024-11-04 18:34:44 +0800

    feat: manage/list开发

--- src/layouts/ManageLayout.tsx
+++ src/layouts/ManageLayout.tsx
@@ -1,14 +1,51 @@
 import React, { FC } from 'react';
 import { Outlet } from 'react-router-dom';
-import styles from '@/assets/styles/ManageLayout.module.scss';
+import { Button, Space, Divider } from 'antd';
+import { PlusOutlined, BarsOutlined, StarOutlined, DeleteOutlined } from '@ant-design/icons';
+import { useNavigate } from 'react-router-dom';
+
+import styles from './ManageLayout.module.scss';
 const ManageLayout: FC = () => {
+  const nav = useNavigate();
+  // const location = useLocation();
+
   return (
     <div className={styles.container}>
-      <div className={styles.left}>ManageLayout left</div>
+      <div className={styles.left}>
+        <Space direction="vertical">
+          <Button type="primary" size="large" icon={<PlusOutlined />}>
+            新建问卷
+          </Button>
+          <Divider style={{ borderColor: 'transparent' }} />
+          <Button
+            type="default"
+            size="large"
+            icon={<BarsOutlined />}
+            onClick={() => nav('/manage/list')}
+          >
+            我的问卷
+          </Button>
+          <Button
+            type="default"
+            size="large"
+            icon={<StarOutlined />}
+            onClick={() => nav('/manage/star')}
+          >
+            星标问卷
+          </Button>
+          <Button
+            type="default"
+            size="large"
+            icon={<DeleteOutlined />}
+            onClick={() => nav('/manage/delete')}
+          >
+            回收站
+          </Button>
+        </Space>
+      </div>
       <div className={styles.right}>
         <Outlet />
       </div>
-      <div>ManageLayout footer</div>
     </div>
   );
 };
