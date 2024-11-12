fad21794360b7e068f4144ed69704bc001fbce96
zhuchunlai <2081659432@qq.com>
2024-11-13 00:07:54 +0800

    feat: 增加logo组件

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -3,11 +3,15 @@ import { Outlet } from 'react-router-dom';
 import { Layout } from 'antd';
 import styles from './MainLayout.module.scss';
 
+import Logo from '@/components/Logo';
+
 const { Header, Content, Footer } = Layout;
 const MainLayout: FC = () => {
   return (
     <Layout>
-      <Header className={styles.header}>MainLayout header</Header>
+      <Header className={styles.header}>
+        <Logo></Logo>
+      </Header>
       <Layout className={styles.main}>
         <Content className={styles.content}>
           <Outlet />
