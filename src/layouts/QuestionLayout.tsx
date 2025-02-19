3078778c6556bfa8d57953980a4830039eab8e57
zhuchunlai <myemail_zhu@163.com>
2025-02-19 18:31:18 +0800

    feat: 开发编辑页面

--- src/layouts/QuestionLayout.tsx
+++ src/layouts/QuestionLayout.tsx
@@ -2,13 +2,9 @@ import React, { FC } from 'react';
 import { Outlet } from 'react-router-dom';
 const QuestionLayout: FC = () => {
   return (
-    <>
-      <div>QuestionLayout header</div>
-      <div>
-        <Outlet />
-      </div>
-      <div>QuestionLayout footer</div>
-    </>
+    <div style={{ height: '100vh' }}>
+      <Outlet />
+    </div>
   );
 };
 
--- src/pages/question/Edit/index.tsx
+++ src/pages/question/Edit/index.tsx
@@ -1,7 +1,21 @@
 import React, { FC } from 'react';
+import styles from '@/assets/styles/pages/question/Edit.module.scss';
 
 const QuestionEdit: FC = () => {
-  return <div>QuestionEdit</div>;
+  return (
+    <div className={styles.container}>
+      <div>Header</div>
+      <div className={styles['content-wrapper']}>
+        <div className={styles.content}>
+          <div className={styles.left}>left</div>
+          <div className={styles.main}>
+            <div className={styles['canvas-wrapper']}>画布</div>
+          </div>
+          <div className={styles.right}>right</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default QuestionEdit;
